d508dfd71f4ad3dc02b8a8a8978a7a3b5320d79a
sloveys <sloveys@mail.uoguelph.ca>
2017-10-21 21:18:43 -0400

    Neurod is functioning!!!?!!!
    Wrote the buildNeuron and runNeuron functions! Everything seems to be
    functioning according to theoretical expectations!

--- NeuronCL.py
+++ NeuronCL.py
@@ -17,40 +17,82 @@ def buildNeuron(context):
       int grid = get_global_id(0);
       des_g[grid] = add_g[grid] + bdd_g[grid];
     }
-    __kernel void mulMatrix(__global const float *aul_g, __global const float *bul_g, __global float *des_g)
+    __kernel void scalarWeight(float weight, __global const float *matrix, __global float *matrixDes)
     {
       int grid = get_global_id(0);
-      des_g[grid] = aul_g[grid] * bul_g[grid];
+      matrixDes[grid] = matrix[grid] * weight;
     }
-    /*__kernel void dot(__global const int *vecSize, __global float *vecDes)
+    __kernel void dotPrime(int vecSize, __global float *vecWeight, __global const float *matrix, __global float *vecDes)
     {
       int grid = get_global_id(0);
-      vecDes[grid] = *vecSize;
-    }*/
+      vecDes[grid] = 0.0;
+      for (int i=0; i < vecSize; i++) {
+        vecDes[grid] += matrix[(vecSize*grid) + i] * vecWeight[i];
+      }
+    }
+    __kernel void dotSecondary(int vecSize, __global float *vecWeight, __global const float *matrix, __global float *vecDes)
+    {
+      int grid = get_global_id(0);
+      vecDes[grid] = 0.0;
+      for (int i=0; i < vecSize; i++) {
+        vecDes[grid] += matrix[(vecSize*i) + grid] * vecWeight[i];
+      }
+    }
+    __kernel void fullMultiply(int vecSize, __global const float *aVec, __global const float *bVec, __global float *matrix)
+    {
+      int grid = get_global_id(0);
+      int x = grid / vecSize;
+      int y = grid % vecSize;
+      matrix[grid] = aVec[x]*bVec[y];
+    }
     """).build()
     return program
-#, __global const float *vecWeight, __global const float *mtx
 
-np.random.seed(42)
+# returns normalized Matrix out
+def runNeuron(queue, program, vectorSize, matrixArr, bias, vectorWeight):
+    mSize = vectorSize*vectorSize
+    inputs = len(matrixArr)
+    weightAve = np.float32(0.0)
+
+    mf = cl.mem_flags
+    vecW_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=vec_np)
+    aVec_g = cl.Buffer(ctx, mf.WRITE_ONLY, 32*vectorSize)
+    bVec_g = cl.Buffer(ctx, mf.WRITE_ONLY, 32*vectorSize)
+    for i in range(inputs):
+        matrix_g = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=matrixArr[i])
+        program.scalarWeight(queue, (mSize,), None, np.float32(bias2[i]), matrix_g, matrix_g)
+        program.dotPrime(queue, (mSize,), None, np.int32(vectorSize), vecW_g, matrix_g, aVec_g)
+        program.dotSecondary(queue, (mSize,), None, np.int32(vectorSize), vecW_g, matrix_g, bVec_g)
+        program.fullMultiply(queue, (mSize,), None, np.int32(vectorSize), aVec_g, bVec_g, matrix_g)
+        weightAve += np.float32(bias[i])
+        if (i == 0):
+            mOut_g = matrix_g
+        else:
+            program.sumMatrix(queue, (mSize,), None, mOut_g, matrix_g, mOut_g)
+
+    weightAve = weightAve/np.float32(inputs)
+    program.scalarWeight(queue, (mSize,), None, np.float32(1/(inputs*weightAve*vectorSize)), mOut_g, mOut_g)
+    mOut_np = np.empty_like(matrixArr[0])
+    cl.enqueue_copy(queue, mOut_np, mOut_g)
+    return mOut_np
+
+np.random.seed(41)
 add_np = np.random.rand(matrixSize).astype(np.float32)
 bdd_np = np.random.rand(matrixSize).astype(np.float32)
-
-vwait - np.random.rand(3).astype(np.float32)
+vec_np = np.random.rand(vectorSize).astype(np.float32)
+bias2 = np.array([1.0, 0.9, 0.8])
+mtrxArr = np.array([add_np, bdd_np])
 
 ctx = cl.create_some_context()
 queue = cl.CommandQueue(ctx)
 
-mf = cl.mem_flags
-add_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=add_np)
-bdd_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=bdd_np)
-
 prg = buildNeuron(ctx)
 
-des_g = cl.Buffer(ctx, mf.WRITE_ONLY, 32*matrixSize) #add_np
-prg.mulMatrix(queue, add_np.shape, None, add_g, bdd_g, des_g)
+#queue, program, vectorSize, matrixArr, bias, vectorWeight
+matrixOut = runNeuron(queue, prg, vectorSize, mtrxArr, bias2, vec_np)
+mtrxArr = np.array([add_np, bdd_np, matrixOut])
+matrixOut2 = runNeuron(queue, prg, vectorSize, mtrxArr, bias2, vec_np)
 
-des_np = np.empty_like(add_np)
-cl.enqueue_copy(queue, des_np, des_g)
 
-# Check on CPU with Numpy:
-print(des_np)
+print(matrixOut)
+print(matrixOut2)
